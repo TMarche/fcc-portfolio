80049afdf78a5f0c6bd9447282c6b77b7a9c6fc1
Troy Marchesseault <tmarche@ncsu.edu>
2021-06-09 14:27:33 -0400

    Add more styling

--- src/components/App.js
+++ src/components/App.js
@@ -42,27 +42,27 @@ const App = () => {
                 <h1 class="section__title section__title--intro">
                     Hi, I am <strong>Jane Smith</strong>
                 </h1>
-                <p class="section__subtitle section__subtitle--intro">front-end dev</p>
-                <img src={devJane01} alt="a picture of Jane Smith smiling"/>
+                <p class="section__subtitle section__subtitle--intro">FRONT-END-DEV</p>
+                <img src={devJane01} alt="a picture of Jane Smith smiling" class="intro__img"/>
             </section>
 
             {/* My Services */}
-            <section class="myservices" id="services">
+            <section class="my-services" id="services">
                 <h2 class="section__title section__title--services">What I do</h2>
                 <div class="services">
                     <div class="service">
                         <h3>Design + Development</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque blandit pharetra erat in semper. Nulla facilisi. Praesent pharetra velit tempor felis tincidunt maximus. Sed eu gravida elit. Suspendisse efficitur tristique nunc, sit amet varius quam consequat in. Nullam varius neque sit amet porta fermentum. Vivamus quis odio at lorem tempor porta. In metus tellus, molestie euismod elit id, semper semper quam. Mauris velit libero, venenatis eu sem in, sagittis volutpat felis. Nullam et velit gravida, mollis sem et, blandit augue. Integer pulvinar vitae libero quis ultricies. Nullam scelerisque turpis eu fermentum efficitur. Sed condimentum eu velit sed feugiat. Curabitur non fermentum justo. Nulla eget egestas felis. Mauris ut mattis enim.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque blandit pharetra erat in semper. Nulla facilisi. Praesent pharetra velit tempor felis tincidunt maximus. Sed eu gravida elit. Suspendisse efficitur tristique nunc, sit amet varius quam consequat in. Nullam varius neque sit amet porta fermentum. Vivamus quis odio at lorem tempor porta.</p>
                     </div> {/* / service */}
                     
                     <div class="service">
                         <h3>E-Commerce</h3>
-                        <p>Donec sodales varius diam vitae porta. Duis vel viverra tortor, eget luctus dolor. Maecenas vitae dictum lacus. Nunc ullamcorper eros nec tellus imperdiet, lobortis scelerisque tortor vestibulum. Nunc eu urna arcu. Sed sit amet ex odio. Curabitur et velit in magna convallis imperdiet. Fusce a magna lectus.</p>
+                        <p>Donec sodales varius diam vitae porta. Duis vel viverra tortor, eget luctus dolor. Maecenas vitae dictum lacus. Nunc ullamcorper eros nec tellus imperdiet, lobortis scelerisque tortor vestibulum. Nunc eu urna arcu. Sed sit amet ex odio. Curabitur et velit in magna convallis imperdiet. Fusce a magna lectus. Nunc eu urna arcu. Sed sit amet ex odio.</p>
                     </div> {/* / service */}
                     
                     <div class="service">
                         <h3>WordPress</h3>
-                        <p>In aliquam lacus at pharetra efficitur. Vivamus consequat ullamcorper sapien, eget lacinia tortor aliquet vitae. Aliquam eleifend est nisi, at sagittis urna ultrices eget. Proin libero nunc, congue non pellentesque nec, pretium eu velit. Integer in felis velit. Morbi eget mi id quam consectetur venenatis. Morbi vitae urna mi. Pellentesque in felis sodales, mattis odio eget, commodo velit. Integer turpis ipsum, viverra nec rutrum non, finibus sed felis. Sed elit nulla, tempor nec lacus et, ornare ultrices lacus. In ultricies lorem tellus, lacinia placerat arcu dapibus eu. Duis malesuada libero nibh, eget varius nunc varius eu. Duis id nunc feugiat, suscipit odio vitae, rutrum odio.</p>
+                        <p>In aliquam lacus at pharetra efficitur. Vivamus consequat ullamcorper sapien, eget lacinia tortor aliquet vitae. Aliquam eleifend est nisi, at sagittis urna ultrices eget. Proin libero nunc, congue non pellentesque nec, pretium eu velit. Integer in felis velit. Morbi eget mi id quam consectetur venenatis. Morbi vitae urna mi.</p>
                     </div> {/* / service */}
                 </div> {/* / services */}
 
@@ -71,21 +71,21 @@ const App = () => {
 
             {/* About Me */}
             <section class="about-me" id="about">
-                <h2 class="section__title section_title--about">Who I am</h2>
+                <h2 class="section__title section__title--about">Who I am</h2>
                 {/* Using p since it's actually better to use p as a subtitle -- using an 'hn' tag is frowned upon here */}
-                <p class = "section__subtitle section__subtitle--about"></p>
+                <p class = "section__subtitle section__subtitle--about">Designer & developer based out of NYC</p>
 
                 <div class="about-me__body">
-                    <p></p>
-                    <p></p>  
+                    <p>Donec sodales varius diam vitae porta. Duis vel viverra tortor, eget luctus dolor. Maecenas vitae dictum lacus. Nunc ullamcorper eros nec tellus imperdiet, lobortis scelerisque tortor vestibulum. Nunc eu urna arcu. Sed sit amet ex odio. Curabitur et velit in magna convallis imperdiet. Fusce a magna lectus. Nunc eu urna arcu. Sed sit amet ex odio.</p>
+                    <p>Donec sodales varius diam vitae porta. Duis vel viverra tortor, eget luctus dolor. Maecenas vitae dictum lacus. Nunc ullamcorper eros nec tellus imperdiet, lobortis scelerisque tortor vestibulum. Nunc eu urna arcu. Sed sit amet ex odio. Curabitur et velit in magna convallis imperdiet. Fusce a magna lectus. Nunc eu urna arcu. Sed sit amet ex odio.</p>  
                 </div>
 
-                <img src={devJane02} alt="Jane leaning against a bus" />
+                <img src={devJane02} alt="Jane leaning against a bus" class="about-me__img" />
             </section>
 
             <section class="my-work" id="work">
-                <h2 class="section__title">My Work</h2>
-                <p class="section__subtitle"></p>
+                <h2 class="section__title section__title--work">My Work</h2>
+                <p class="section__subtitle section__subtitle--work">A selection of my range of work</p>
 
                 {/* Portfolio item 1 */}
                 <div class="portfolio">
